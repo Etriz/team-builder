bfef09c73b8e8b94f44f1d0e2d8ff450b7a4ecda
ryanp <ryanpanimation@gmail.com>
2020-04-25 02:01:36 -0500

    styles with tailwind

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from "react";
 import Form from "./Components/Form";
-import "./App.css";
+// import "./App.css";
 
 function App() {
   const FALLBACK_MEMBERS = [
-    { name: "Pajama Sam", email: "sam@gmail.com", role: "Pajama Man" },
-    { name: "Scuba Steve", email: "steve@scuba.com", role: "Scuba Instructor" },
+    { id: 98, name: "Pajama Sam", email: "sam@gmail.com", username: "PajamaMan" },
+    { id: 99, name: "Scuba Steve", email: "steve@scuba.com", username: "ScubaProf" },
   ];
   const [allMembers, setAllMembers] = useState(FALLBACK_MEMBERS);
 
@@ -22,13 +22,19 @@ function App() {
       .catch((err) => console.error("Fetch API error!", err));
   }, []);
   return (
-    <div className="App">
-      <div className="App-header">
+    <div className="App container mx-auto my-4 text-white">
+      <h2 className="text-center text-4xl">Team Members</h2>
+      <div className="App-header flex flex-wrap justify-center">
         {allMembers.map((item) => (
-          <p key={item.name}>{item.name}}</p>
+          <div key={item.id} className="w-56 h-22 m-2 p-2 rounded border border-white bg-teal-700">
+            <h2>{item.name}</h2>
+            <hr />
+            <p>{item.email}</p>
+            <p>{item.username}</p>
+          </div>
         ))}
-        <Form addTeamMember={addTeamMember} />
       </div>
+      <Form addTeamMember={addTeamMember} />
     </div>
   );
 }
--- team-builder/src/Components/Form.js
+++ team-builder/src/Components/Form.js
@@ -1,47 +1,70 @@
 import React, { useState } from "react";
 
 const Form = ({ addTeamMember }) => {
-  const [member, setMember] = useState({ name: "", email: "", role: "" });
+  const BLANK_MEMBER = { id: Date.now(), name: "", email: "", username: "" };
+  const [member, setMember] = useState(BLANK_MEMBER);
 
   const submitForm = (event) => {
     event.preventDefault();
-    addTeamMember(member);
-    setMember({ name: "", email: "", role: "" });
+    if (
+      member.name !== BLANK_MEMBER.name &&
+      member.email !== BLANK_MEMBER.email &&
+      member.username !== BLANK_MEMBER.username
+    ) {
+      addTeamMember(member);
+      setMember(BLANK_MEMBER);
+    }
   };
   const handleChanges = (event) => {
-    setMember({ ...member, [event.target.name]: event.target.value });
+    // console.log(event.target.value);
+    setMember({ ...member, [event.target.name]: event.target.value.toString() });
   };
 
   return (
-    <form onSubmit={submitForm}>
-      <label htmlFor="name">Name</label>
-      <input
-        id="name"
-        type="text"
-        placeholder="Add Name"
-        value={member.name}
-        name="name"
-        onChange={handleChanges}
-      />
-      <label htmlFor="email">Email</label>
-      <input
-        id="email"
-        type="email"
-        placeholder="Add Email"
-        value={member.email}
-        name="email"
-        onChange={handleChanges}
-      />
-      <label htmlFor="role">Role</label>
-      <input
-        id="role"
-        type="text"
-        placeholder="Add Role"
-        value={member.role}
-        name="role"
-        onChange={handleChanges}
-      />
-      <button type="submit">Submit</button>
+    <form
+      onSubmit={submitForm}
+      className="rounded border border-white bg-teal-900 p-6 mx-auto my-2 max-w-md">
+      <div className="flex justify-between">
+        <label htmlFor="name">Name</label>
+        <input
+          id="name"
+          type="text"
+          placeholder="Add Name"
+          value={member.name}
+          name="name"
+          className="px-2 py-1 ml-6 text-black border-2 outline-none focus:border-orange-600"
+          onChange={handleChanges}
+        />
+      </div>
+      <div className="my-6 flex justify-between">
+        <label htmlFor="email">Email</label>
+        <input
+          id="email"
+          type="email"
+          placeholder="Add Email"
+          value={member.email}
+          name="email"
+          className="px-2 py-1 ml-6 text-black border-2 outline-none focus:border-orange-600"
+          onChange={handleChanges}
+        />
+      </div>
+      <div className="flex justify-between">
+        <label htmlFor="username">Username</label>
+        <input
+          id="username"
+          type="text"
+          placeholder="Add Username"
+          value={member.username}
+          name="username"
+          className="px-2 py-1 ml-6 text-black border-2 outline-none focus:border-orange-600"
+          onChange={handleChanges}
+        />
+      </div>
+      <button
+        type="submit"
+        className="bg-white text-black rounded border py-1 px-3 mt-6 w-1/2 hover:bg-orange-600 hover:border-white hover:text-white">
+        Submit
+      </button>
     </form>
   );
 };
